7f853d05df4e0ed0a139626cdd3038103c3c530f
nextacademy <nextacademy@email.com>
2015-12-08 14:04:37 +0800

    2pm deadline but not yet done. December bacth awaits

--- app/controllers/bookings.rb
+++ app/controllers/bookings.rb
@@ -1,3 +1,14 @@
+# Display new booking form
+get '/bookings/new' do
+	erb :"bookings/new"
+end
+
+# View All bookings
+get '/bookings/index' do
+	erb :'bookings/index'
+end
+
+
 # Create new booking
 post '/bookings' do
 	booking = Booking.create(title: params[:title], body: params[:body], user_id: session[:user_id], property_id: params[:property_id])
@@ -5,10 +16,7 @@ post '/bookings' do
 	# erb :"bookings/new"	
 end
 
-# View All bookings
-get '/bookings/index' do
-	erb :'bookings/index'
-end
+
 
 # View booking
 get '/bookings/:id' do
@@ -21,3 +29,17 @@ get '/bookings/:id/edit' do
 	@booking = Booking.find(params[:id])
 	erb :'bookings/edit'
 end 
+
+# Update booking
+patch '/bookings/:id' do
+	booking = Booking.find(params[:id])
+	booking.update(title: params[:title], body: params[:body])
+	redirect "/bookings/#{booking.id}"
+end
+
+# Delete booking
+delete '/bookings/:id' do
+	booking = Booking.find(params[:id])
+	booking.destroy
+	erb :'static/index'
+end
--- app/controllers/comments.rb
+++ app/controllers/comments.rb
@@ -1,3 +1,13 @@
+# Display new comments form
+get '/comments/new' do
+	erb :"comments/new"
+end
+
+# View All comments
+get '/comments/index' do
+	erb :'comments/index'
+end
+
 # Create new comments
 post '/comments' do
 	comment = Comment.create(title: params[:title], body: params[:body], user_id: session[:user_id], property_id: params[:property_id])
@@ -5,10 +15,6 @@ post '/comments' do
 	# erb :"comments/new"	
 end
 
-# View All comments
-get '/comments/index' do
-	erb :'comments/index'
-end
 
 # View comments
 get '/comments/:id' do
@@ -21,3 +27,17 @@ get '/comments/:id/edit' do
 	@comment = Comment.find(params[:id])
 	erb :'comments/edit'
 end 
+
+# Update comments
+patch '/comments/:id' do
+	comment = Comment.find(params[:id])
+	comment.update(title: params[:title], body: params[:body])
+	redirect "/comments/#{comment.id}"
+end
+
+# Delete comment
+delete '/comments/:id' do
+	comment = Comments.find(params[:id])
+	comment.destroy
+	erb :'static/index'
+end
